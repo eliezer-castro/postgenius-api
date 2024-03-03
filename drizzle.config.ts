fe0672598da05f14e7517f81ff238170fea1f3be
Eliezer Gino <eliezergino@gmail.com>
2024-03-03 14:46:53 -0300

    Configurando Drizzle ORM

---
+++ drizzle.config.ts
@@ -0,0 +1,9 @@
+import type { Config } from "drizzle-kit";
+export default {
+  schema: "./src/db/schema/index.ts",
+  out: "./drizzle",
+  driver: "pg",
+  dbCredentials: {
+    connectionString: 'postgres://docker:docker@localhost:5432/postgenius'
+  }
+} satisfies Config;
